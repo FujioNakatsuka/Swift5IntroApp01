579e8c6bdb21d8bd3162d21e877ee88cd4c55610
Fujio Nakatsuka <com.nifty.cbm01799>
2020-08-27 07:33:53 +0900

    353「ここまでを確認しよう」まで。 ビルドはできるのですが、アニメーションが出ません。 スキップボタンを所定の位置の右上に置くと、scrollVieewの下に入ってしまいます。その後、再びボタンの位置が中央に戻ります。

--- Swift5IntroApp01/Controller/introViewController.swift
+++ Swift5IntroApp01/Controller/introViewController.swift
@@ -9,31 +9,63 @@
 import UIKit
 import Lottie
 
-class introViewController: UIViewController {
+class introViewController: UIViewController,UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     
     var onboardArray = ["1","2","3","4","5"]
     
-    var onboardAtringArray = ["We are the world! ","We are the friends!","We a the children!","We are the human!","We are the citizen!"]
+    var onboardStringArray = ["We are the world! ","We are the friends!","We a the children!","We are the human!","We are the citizen!"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.isPagingEnabled = true
+        setUpScroll()
+        
+        for i in 0...4{
+            
+            let animationView = AnimationView()
+            
+            //viewのsizeの分だけ、右へいく
+            let animation = Animation.named(onboardStringArray[i])
+            animationView.frame = CGRect(x: CGFloat(i) * view.frame.size.width, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+            animationView.animation = animation
+            animationView.contentMode = .scaleAspectFit
+            animationView.loopMode = .loop
+            animationView.play()
+            
+            scrollView.addSubview(animationView)
+     
+        }
         
         
     }
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         self.navigationController?.isNavigationBarHidden = true
     }
     
     func setUpScroll(){
         
+        scrollView.delegate = self
         
+        //pagingのために×5（Array内の要素の数）
+        scrollView.contentSize = CGSize(width: view.frame.size.width * 5, height: scrollView.frame.size.height)
+    
+        for i in 0...4{
         
-    }
+        let onboardLabel = UILabel(frame:CGRect(x: CGFloat(i) * view.frame.size.width, y: view.frame.size.height/3, width: scrollView.frame.size.width, height: scrollView.frame.size.height))
+        
+        onboardLabel.font = UIFont.boldSystemFont(ofSize: 15.0)
+            onboardLabel.textAlignment = .center
+            onboardLabel.text = onboardStringArray[i]
+            scrollView.addSubview(onboardLabel)
+        
+        }
     
     
     
 
+    }
 }
